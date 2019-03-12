b1a6ebd3da0f062f6d942b1a5a0b8519ed4e679c
Antonio Garcia <a.garcia@hp.com>
2019-03-12 21:54:59 +0100

    Add depemdencies to page grid

--- src/frontend/App/Ng-Components/pageGrid/page-grid.component.ng2.ts
+++ src/frontend/App/Ng-Components/pageGrid/page-grid.component.ng2.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { PageFields } from "../../../../common/model";
+import { ISelectableOption } from "../../../../common/rest";
+import { DataService } from "../../Services/data.service";
+import { LocalizationService } from "../localization.service";
 
 @Component({
     selector: "page-grid",
@@ -6,9 +11,43 @@ import { Component, OnInit } from "@angular/core";
 })
 
 export class PageGridComponent implements OnInit {
-// tslint:disable-next-line: no-empty
-    constructor() { }
 
-// tslint:disable-next-line: no-empty
-    public ngOnInit() { }
+    /**
+     * Internal properties
+     */
+    private selectedPages: number[] = [];
+    private localizedPrintQualityCapabilities: ISelectableOption[] = [];
+    private localizedMediaTypeCapabilities: ISelectableOption[] = [];
+    private localizedDestinationCapabilities: ISelectableOption[] = [];
+    private localizationSubscription: Subscription;
+
+    constructor(
+        private dataService: DataService,
+        private localizationService: LocalizationService
+    ) {}
+
+    public ngOnInit() {
+        this.loadLocalizedCapabilities();
+    }
+
+    /**
+     * Gets the capabilites that need to be translated and translates them
+     */
+    private loadLocalizedCapabilities() {
+        this.localizedPrintQualityCapabilities = [];
+        this.dataService
+            .getCapabilities(PageFields.PrintQuality)
+            .map((capability) => this.localizationService.getLocalizedCapability(capability)
+                .subscribe((result) => this.localizedPrintQualityCapabilities.push(result)));
+        this.localizedMediaTypeCapabilities = [];
+        this.dataService
+            .getCapabilities(PageFields.MediaType)
+            .map((capability) => this.localizationService.getLocalizedCapability(capability)
+                .subscribe((result) => this.localizedMediaTypeCapabilities.push(result)));
+        this.localizedDestinationCapabilities = [];
+        this.dataService
+            .getCapabilities(PageFields.Destination)
+            .map((capability) => this.localizationService.getLocalizedCapability(capability)
+                .subscribe((result) => this.localizedDestinationCapabilities.push(result)));
+    }
 }
--- src/frontend/App/Ng-Components/pageGrid/page-grid.component.spec.ts
+++ src/frontend/App/Ng-Components/pageGrid/page-grid.component.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from "@angular/core/testing";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { Observable, of } from "rxjs";
+import { AppServicesModule } from "../../Services";
 import { PageGridComponent } from "./page-grid.component.ng2";
 
 describe("Given a page grid controller", () => {
 
+    const translations: any = { STR_Any: "This is a test" };
+
     let element: Element;
 
+    class FakeLoader implements TranslateLoader {
+        public getTranslation(lang: string): Observable<any> {
+            return of(translations);
+        }
+    }
+
     beforeEach(() => {
-        TestBed.configureTestingModule({ declarations: [ PageGridComponent ] });
+        TestBed.configureTestingModule({
+            declarations: [ PageGridComponent ],
+            imports: [
+                AppServicesModule,
+                TranslateModule.forRoot({
+                    loader: { provide: TranslateLoader, useClass: FakeLoader}
+                })
+             ]
+        });
         const fixture = TestBed.createComponent(PageGridComponent);
         element = fixture.nativeElement;
     });
