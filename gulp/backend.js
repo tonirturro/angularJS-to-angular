10080ad6be674e98cda9b8806005d98bf9e45120
Garcia <a.garcia@hp.com>
2019-07-08 16:04:47 +0200

    Webpack fully replaced

--- gulp/backend.js
+++ gulp/backend.js
@@ -1,14 +1,10 @@
 const gulp = require('gulp');
-const path = require('path');
-const webpack = require('webpack-stream');
-const webpackConfigBackend = require('../webpack/webpack.config.backend');
+const exec = require('child_process').exec;
 
-const backendOutput = path.resolve(__dirname, '../backend-build');
-const backendSources = path.resolve(__dirname, '../src/backend');
-const backendMain = path.resolve(backendSources, 'main.ts');
-
-gulp.task('backend', () => {
-    return gulp.src(backendMain)
-    .pipe(webpack(webpackConfigBackend))
-    .pipe(gulp.dest(backendOutput));
+gulp.task('backend', (cb) => {
+    exec('ng run backend:build', (err, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+      });    
 });
--- webpack/webpack.config.backend.js
+++ webpack/webpack.config.backend.js
@@ -5,22 +5,5 @@ module.exports =  {
     },
     target: 'electron-main',
     mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [{
-                    loader: 'ts-loader',
-                    options: {
-                        transpileOnly: true,
-                        experimentalWatchApi: true
-                    }
-                }]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.ts']
-    },
     devtool: 'inline-source-map'
 }
